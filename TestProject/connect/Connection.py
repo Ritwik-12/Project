d54b86ed2be76affe5936e679282e6455ae4592c
Ritwik <ritwikbhowmik659@gmail.com>
2023-06-21 22:09:49 +0530

    Removed

--- TestProject/connect/Connection.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-class Connect1:
-    def conn(self):
-        self.con=pymysql.connect(host="localhost",user="root",\
-                    password='',database='examdb',\
-                    charset='utf8mb4',\
-                    cursorclass=pymysql.cursors.DictCursor)
-        
-        return self.con
